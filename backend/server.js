d3b742f51c7a4b1e71828b29b9969a01b245936d
Folastar <ogundelefolariin@gmail.com>
2025-03-21 19:37:23 +0100

    trail fix

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app =express()
 
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://agrihaven.onrender.com"],
+    origin: ["http://localhost:5173", "https://agri-haven-lbyj.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
     credentials: true, // Allow cookies if needed
   }));
